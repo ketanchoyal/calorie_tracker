5369bcc82e9f2fc62d0336c92badec68ad9eab40
Ketan Choyal <sharmaketanr@gmail.com>
2022-06-26 12:59:57 -0400

    DateTime parsing bug fixed

--- .vscode/flutter_web_deploy.sh
+++ .vscode/flutter_web_deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+#Script to deploy to flutter web
+
 echo "Installing pub dependencies"
 flutter pub get
+echo "Building Web"
 flutter build web --target lib/main_production.dart
+echo "Deploying to Firebase"
 flutter deploy
